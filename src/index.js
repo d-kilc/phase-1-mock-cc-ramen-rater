69754633958618c914ee9d79b2ec835c56b7c15f
Daniel <dkilcullen25@gmail.com>
2021-12-22 13:04:11 -0600

    all deliverables

--- src/index.js
+++ src/index.js
@@ -2,9 +2,8 @@
 function init() {
     fetch('http://localhost:3000/ramens')
     .then((res) => res.json())
-    .then((json) => {
-        renderRamen(json)
-    })
+    .then(renderRamen)
+    .catch(() => alert('Something\'s wrong.'))
 }
 
 function renderRamen(ramen) {
@@ -15,37 +14,32 @@ function renderRamen(ramen) {
     document.querySelector('#comment-display').textContent = ramen[0].comment
 
     ramen.forEach((obj) => {
-        const img = document.createElement('img')
-        
+        const img = document.createElement('img')        
         img.src = obj.image
         img.id = obj.id
-        
-        document.querySelector('#ramen-menu').appendChild(img)
+
+        const parentDiv = document.createElement('div')
+        document.querySelector('#ramen-menu').appendChild(parentDiv)
 
         img.addEventListener('click', (e) => {
             document.querySelector('.detail-image').src = obj.image
+            document.querySelector('.detail-image').id = obj.id
             document.querySelector('.name').textContent = obj.name
             document.querySelector('.restaurant').textContent = obj.restaurant
             document.querySelector('#rating-display').textContent = obj.rating
             document.querySelector('#comment-display').textContent = obj.comment
-
-            const editForm = document.createElement('form')
-            editForm.id = 'edit-form'
-
         })
+        parentDiv.appendChild(img)
+
+        const deleter = document.createElement('div')
+        deleter.textContent = 'X'
+
+        deleter.addEventListener('click', deleteRamen)
+        parentDiv.appendChild(deleter)
     })   
 }
 
-document.querySelector('#edit-ramen').addEventListener('submit', (e) => {
-    e.preventDefault()
-    
-    document.querySelector('#rating-display').textContent = e.target[0].value
-    document.querySelector('#comment-display').textContent = e.target[1].value
-
-    e.target.reset()
-})
-
-document.querySelector('#new-ramen').addEventListener('submit',(e) => {
+function postNewRamen(e) {
     e.preventDefault()
 
     const newRamen = {
@@ -56,27 +50,67 @@ document.querySelector('#new-ramen').addEventListener('submit',(e) => {
         comment: e.target[4].value
     }
 
-    const img = document.createElement('img')
-    img.src = newRamen.image
-    img.id = newRamen.id
-    document.querySelector('#ramen-menu').appendChild(img)
-
-    img.addEventListener('click', (e) => {
-        document.querySelector('.detail-image').src = newRamen.image
-        document.querySelector('.name').textContent = newRamen.name
-        document.querySelector('.restaurant').textContent = newRamen.restaurant
-        document.querySelector('#rating-display').textContent = newRamen.rating
-        document.querySelector('#comment-display').textContent = newRamen.comment
+    fetch('http://localhost:3000/ramens', {
+        method: 'POST',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: 'application/json'
+        },
+        body: JSON.stringify(newRamen)
     })
-
-    // fetch('http://localhost:3000/ramens', {
-    //     method: 'post',
-    //     body: JSON.stringify(newRamen)
-    // })
-    // .then((res)=>res.json())
-    // .then((json) => console.log(json))
+    .then(() => alert('Ramen posted! Pls refresh.'))
+    .catch(() => alert('Couldn\'t post.'))
 
     e.target.reset()
-})
+    
+    // how to reset menu pics without refreshing?
+    // init()
+}
+
+function editRamen(e) {    
+    e.preventDefault()
+
+    document.querySelector('#rating-display').textContent = e.target[0].value
+    document.querySelector('#comment-display').textContent = e.target[1].value
+    const newRating = e.target[0].value
+    const newComment = e.target[1].value
+    const updatedId = document.querySelector('.detail-image').id
+console.log(document.querySelector('.detail-image'))
+    console.log(updatedId)
+
+    //show the update from inputs (until they refresh)
+    document.querySelector('#rating-display').textContent = newRating
+    document.querySelector('#comment-display').textContent = newComment
+
+    //persist changes
+    fetch(`http://localhost:3000/ramens/${updatedId}`, {
+        method: 'PATCH',
+        headers: {
+            "Content-Type": "application/json",
+            Accept: "application/json"
+        },
+        body: JSON.stringify({
+            rating: newRating,
+            comment: newComment
+        })
+    })
+    .then(() => alert('Ramen updated! Pls refresh'))
+    .catch(() => alert('Couldn\'t update.'))
+
+    e.target.reset()
+}
+
+function deleteRamen(e) {
+    const ramen = e.target.previousSibling
+    console.log(ramen)
+    fetch(`http://localhost:3000/ramens/${ramen.id}`, {
+        method: 'DELETE'
+    })
+    .then(() => alert('Ramen deleted! Pls refresh'))
+    .catch(() => Error('Couldn\'t delete.'))
+}
+
+document.querySelector('#edit-ramen').addEventListener('submit', editRamen)
+document.querySelector('#new-ramen').addEventListener('submit', postNewRamen)
 
 init()
